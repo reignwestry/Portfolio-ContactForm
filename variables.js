b678bf807f0b4c0dcf1667a2c2b5036e97562bc1
18231550+reignreapsow@users.noreply.github.com <18231550+reignreapsow@users.noreply.github.com>
2020-07-29 11:44:20 -0400

    GITIGNORE update

---
+++ variables.js
@@ -0,0 +1,12 @@
+const host = 'mail.reignwestry.com';
+const username = 'remotedev@reignwestry.com';
+const pass = '@reignDI1782';
+
+//mailOptions 
+const senderEmail = 'test@testaccount.com';
+const receiverEmail = 'remotedev@reignwestry.com';
+const replyToEmail = 'test@testaccount.com';
+
+
+//MESSAGE DATA
+const messageSubject = 'NEW PORTFOLIO CONTACT FORM TEST MESSAGE'
